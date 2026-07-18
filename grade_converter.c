cfe283d0c2d806f3d8ec1a64fe4891c4f2c2caff
FAD_ADS <fuadadi137@gmail.com>
2026-07-19 00:44:07 +0700

    Implement grade conversion program for subjects
    This program allows users to input a grade and receive a corresponding letter grade based on their input for two subjects.

---
+++ grade_converter.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+    int nilai;
+    char matkul;
+    printf("Program Nilai Mata  Kuliah");
+    printf("\n------------------------");
+    printf("\t\n-----Dasar Matkul-----" );
+    printf("\nA. Dasar Pemrograman Teori");
+    printf("\nB. Dasar Pemrograman Praktikum");
+    printf("\nPilih matkul [A/B]: ");
+    scanf("%s", &matkul);
+
+    switch (toupper(matkul))
+    {
+        case 'A':
+        printf("\nMasukkan Nilai : ");
+        scanf("%d", &nilai);
+        switch  (nilai/10)
+        {
+            case 1:
+            case 2:
+                printf("\nNilai Anda E\n");
+                break;
+            case 3:
+            case 4:
+                printf("\nNilai Anda D\n");
+                break;
+            case 5:
+            case 6:
+                printf("\nNilai Anda C\n");
+                break;
+            case 7:
+            case 8:
+                printf("\nNilai Anda B\n");
+                break;
+            case 9:
+            case 10:
+                printf("\nNilai Anda A\n");
+            default : printf("\nsalah input");
+        }
+        break;
+
+        case 'B':
+        printf("\nMasukkan Nilai : ");
+        scanf("%d", &nilai);
+        switch  (nilai/10)
+        {
+            case 1:
+            case 2:
+                printf("\nNilai Anda E\n");
+                break;
+            case 3:
+            case 4:
+                printf("\nNilai Anda D\n");
+                break;
+            case 5:
+            case 6:
+                printf("\nNilai Anda C\n");
+                break;
+            case 7:
+            case 8:
+                printf("\nNilai Anda B\n");
+                break;
+            case 9:
+            case 10:
+                printf("\nNilai Anda A\n");
+            default : printf("\nsalah input");
+        }
+        break;
+
+        default: printf("\nsalah input");
+        break;
+    }
+}
